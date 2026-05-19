6eb8b2f74adc2c6cab5557fa3a3fc5246fbd340c
Patrik Říha <dogyczsk@Patrik--MacBook-Pro.local>
2026-05-19 08:21:27 +0200

    newProject

---
+++ src/Game/GameException.java
@@ -0,0 +1,5 @@
+package Game;
+
+public class GameException  extends Exception{
+
+}
---
+++ src/Game/GamePanel.java
@@ -0,0 +1,7 @@
+package Game;
+
+import javax.swing.*;
+
+public class GamePanel extends JPanel implements Renderable {
+
+}
